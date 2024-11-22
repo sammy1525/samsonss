a17ff1d7564980bd5fa2247b44fc0d3f9c241528
sammy1525 <mulumba1525@gmail.com>
2024-11-22 08:51:09 +0300

    Update FiveStudentsTest.java

--- app/src/test/java/edu/umn/cs/csci3081w/lab11/FiveStudentsTest.java
+++ app/src/test/java/edu/umn/cs/csci3081w/lab11/FiveStudentsTest.java
@@ -2,43 +2,138 @@ package edu.umn.cs.csci3081w.lab11;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.verify;
 
 import com.google.gson.JsonObject;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
 
 import javax.websocket.Session;
 
 public class FiveStudentsTest {
 
   @Test
-  public void testHamburgerWithGrilledMushrooms() {
-    //we are using the Mockito spy API method as we want to
-    //preserve the implementation of the methods inside FiveStudentsWebServer.
-    //Please note that the spy method does not mean that we are creating a
-    // test double of type spy
-    FiveStudentsWebServer fiveStudentsWebServer = spy(FiveStudentsWebServer.class);
-    //tell the test double to not do anything when the method sendJson is
-    //called with a JSON object parameter
-    doNothing().when(fiveStudentsWebServer).sendJson(Mockito.isA(JsonObject.class));
-    //create a dummy object for the session as we need it to open the session
+  public void testValidOrder_HamburgerWithGrilledMushrooms() {
+    FiveStudentsWebServer server = spy(FiveStudentsWebServer.class);
+    doNothing().when(server).sendJson(isA(JsonObject.class));
     Session sessionDummy = mock(Session.class);
-    //open session
-    fiveStudentsWebServer.onOpen(sessionDummy);
-    //prepare input to send to fiveStudentsWebServer
+    server.onOpen(sessionDummy);
+
     JsonObject order = new JsonObject();
     order.addProperty("text", "hamburger grilledmushrooms");
-    //send the input to the FiveStudents software
-    fiveStudentsWebServer.onMessage(order.toString());
-    //capture the JSON object passed to the sendJson method of the fiveStudentsWebServer
-    ArgumentCaptor<JsonObject> messageCaptor = ArgumentCaptor.forClass(JsonObject.class);
-    verify(fiveStudentsWebServer).sendJson(messageCaptor.capture());
-    //check that we have the expected output
-    JsonObject message = messageCaptor.getValue();
+    server.onMessage(order.toString());
+
+    ArgumentCaptor<JsonObject> captor = ArgumentCaptor.forClass(JsonObject.class);
+    verify(server).sendJson(captor.capture());
+    JsonObject response = captor.getValue();
     String expected = "Description:hamburger grilled mushrooms Calories:890";
-    assertEquals(expected, message.get("text").getAsString());
+    assertEquals(expected, response.get("text").getAsString());
   }
 
+  @Test
+  public void testValidOrder_HotDogWithMultipleToppings() {
+    FiveStudentsWebServer server = spy(FiveStudentsWebServer.class);
+    doNothing().when(server).sendJson(isA(JsonObject.class));
+    Session sessionDummy = mock(Session.class);
+    server.onOpen(sessionDummy);
+
+    JsonObject order = new JsonObject();
+    order.addProperty("text", "hotdog grilledonions,grilledmushrooms,jalapenopeppers");
+    server.onMessage(order.toString());
+
+    ArgumentCaptor<JsonObject> captor = ArgumentCaptor.forClass(JsonObject.class);
+    verify(server).sendJson(captor.capture());
+    JsonObject response = captor.getValue();
+    String expected = "Description:hotdog grilled onions grilled mushrooms jalapeno peppers Calories:555";
+    assertEquals(expected, response.get("text").getAsString());
+  }
+
+  @Test
+  public void testValidOrder_SandwichWithoutToppings() {
+    FiveStudentsWebServer server = spy(FiveStudentsWebServer.class);
+    doNothing().when(server).sendJson(isA(JsonObject.class));
+    Session sessionDummy = mock(Session.class);
+    server.onOpen(sessionDummy);
+
+    JsonObject order = new JsonObject();
+    order.addProperty("text", "sandwich");
+    server.onMessage(order.toString());
+
+    ArgumentCaptor<JsonObject> captor = ArgumentCaptor.forClass(JsonObject.class);
+    verify(server).sendJson(captor.capture());
+    JsonObject response = captor.getValue();
+    String expected = "Description:sandwich Calories:290";
+    assertEquals(expected, response.get("text").getAsString());
+  }
+
+  @Test
+  public void testInvalidOrder_UnknownFoodItem() {
+    FiveStudentsWebServer server = spy(FiveStudentsWebServer.class);
+    doNothing().when(server).sendJson(isA(JsonObject.class));
+    Session sessionDummy = mock(Session.class);
+    server.onOpen(sessionDummy);
+
+    JsonObject order = new JsonObject();
+    order.addProperty("text", "pizza grilledmushrooms");
+    server.onMessage(order.toString());
+
+    ArgumentCaptor<JsonObject> captor = ArgumentCaptor.forClass(JsonObject.class);
+    verify(server).sendJson(captor.capture());
+    JsonObject response = captor.getValue();
+    String expected = "we do not have this type of fast food item";
+    assertEquals(expected, response.get("text").getAsString());
+  }
+
+  @Test
+  public void testInvalidOrder_WrongFormat() {
+    FiveStudentsWebServer server = spy(FiveStudentsWebServer.class);
+    doNothing().when(server).sendJson(isA(JsonObject.class));
+    Session sessionDummy = mock(Session.class);
+    server.onOpen(sessionDummy);
+
+    JsonObject order = new JsonObject();
+    order.addProperty("text", "hamburger grilledmushrooms extra");
+    server.onMessage(order.toString());
+
+    ArgumentCaptor<JsonObject> captor = ArgumentCaptor.forClass(JsonObject.class);
+    verify(server).sendJson(captor.capture());
+    JsonObject response = captor.getValue();
+    String expected = "the fast food item order is not right";
+    assertEquals(expected, response.get("text").getAsString());
+  }
+
+  @Test
+  public void testEdgeCase_EmptyOrder() {
+    FiveStudentsWebServer server = spy(FiveStudentsWebServer.class);
+    doNothing().when(server).sendJson(isA(JsonObject.class));
+    Session sessionDummy = mock(Session.class);
+    server.onOpen(sessionDummy);
+
+    JsonObject order = new JsonObject();
+    order.addProperty("text", "");
+    server.onMessage(order.toString());
+
+    ArgumentCaptor<JsonObject> captor = ArgumentCaptor.forClass(JsonObject.class);
+    verify(server).sendJson(captor.capture());
+    JsonObject response = captor.getValue();
+    String expected = "the fast food item order is not right";
+    assertEquals(expected, response.get("text").getAsString());
+  }
+
+  @Test
+  public void testEdgeCase_EmptyToppings() {
+    FiveStudentsWebServer server = spy(FiveStudentsWebServer.class);
+    doNothing().when(server).sendJson(isA(JsonObject.class));
+    Session sessionDummy = mock(Session.class);
+    server.onOpen(sessionDummy);
+
+    JsonObject order = new JsonObject();
+    order.addProperty("text", "hotdog ");
+    server.onMessage(order.toString());
+
+    ArgumentCaptor<JsonObject> captor = ArgumentCaptor.forClass(JsonObject.class);
+    verify(server).sendJson(captor.capture());
+    JsonObject response = captor.getValue();
+    String expected = "Description:hotdog Calories:520";
+    assertEquals(expected, response.get("text").getAsString());
+  }
 }
